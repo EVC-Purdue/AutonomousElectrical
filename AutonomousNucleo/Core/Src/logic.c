2c72e4c3768d2e418db515e827cb8b9b0bb48707
Millan Kumar <48894200+LelsersLasers@users.noreply.github.com>
2025-11-30 20:00:34 -0500

    Use LED to blink state (running, contactor, SPI)

--- AutonomousNucleo/Core/Src/logic.c
+++ AutonomousNucleo/Core/Src/logic.c
@@ -11,6 +11,7 @@
 
 static uint32_t gs_boot_time   = 0;
 static bool gs_contactor_state = false;
+static bool spi_okay 	       = true;
 
 static uint8_t gs_rx_buff[SPI_MSG_SIZE] = {0};
 static uint8_t gs_tx_buff[SPI_MSG_SIZE] = {0};
@@ -20,6 +21,8 @@ void logic_init() {
 	// Start by setting contactor to false
 	gs_contactor_state = false;
 	HAL_GPIO_WritePin(CONTACTOR_GPIO_Port, CONTACTOR_Pin, GPIO_PIN_RESET);
+	// Turn off LED initially
+	HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 }
 
 void logic_run(
@@ -45,6 +48,22 @@ void logic_run(
 		// Force set speed to 0
 		gs_rx_buff[0] = 0;
 		gs_rx_buff[1] = 0;
+		spi_okay = false;
+	} else {
+		spi_okay = true;
+	}
+
+	// Set LED state
+	if (gs_contactor_state && spi_okay) {
+		// Loop rate is bound/delayed by blocking SPI call
+		// Toggle basically on SPI message
+		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+	} else if (gs_contactor_state) {
+		// Set when contactor on but SPI timeout
+		HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
+	} else {
+		// SPI is not RXing okay and contactor off
+		HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 	}
 
 	// Process received data
