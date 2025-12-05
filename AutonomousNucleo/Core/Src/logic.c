fc7675e79b4b5a136babdff54675ddffde320703
Millan Kumar <48894200+LelsersLasers@users.noreply.github.com>
2025-12-05 14:43:32 -0500

    removed unused variable

--- AutonomousNucleo/Core/Src/logic.c
+++ AutonomousNucleo/Core/Src/logic.c
@@ -17,9 +17,8 @@ static uint8_t gs_rx_buff[SPI_MSG_SIZE] = {0};
 static uint8_t gs_tx_buff[SPI_MSG_SIZE] = {0};
 
 // RC E-stop PWM interrupt ---------------------------------------------------//
-static volatile uint32_t gs_tim5_ic_rising      = 0;
-static volatile uint32_t gs_tim5_pulse_width_us = 0;
-static volatile bool gs_tim5_capturing          = false;
+static volatile uint32_t gs_tim5_ic_rising = 0;
+static volatile bool gs_tim5_capturing     = false;
 
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef* htim) {
     if ((htim->Instance == TIM5) && (htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)) {
