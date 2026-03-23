3319f401369e929aea7e59944587b880fd55d16f
LelsersLasers <millankumar@gmail.com>
2026-03-23 15:24:20 -0400

    Fixing TIM1 and TIM11 prescaler?
    APB2TimFreq_Value=120000000 ??

--- AutonomousDistroBoard/Core/Src/main.c
+++ AutonomousDistroBoard/Core/Src/main.c
@@ -223,7 +223,7 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
-  htim1.Init.Prescaler = 89;
+  htim1.Init.Prescaler = 119;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim1.Init.Period = 19999;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -286,7 +286,7 @@ static void MX_TIM11_Init(void)
 
   /* USER CODE END TIM11_Init 1 */
   htim11.Instance = TIM11;
-  htim11.Init.Prescaler = 89;
+  htim11.Init.Prescaler = 119;
   htim11.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim11.Init.Period = 19999;
   htim11.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
