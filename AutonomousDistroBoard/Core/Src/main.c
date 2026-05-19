3c7158071d22c11d16d0b50b2460fa8255eaa33a
LelsersLasers <millankumar@gmail.com>
2026-05-19 09:19:01 -0400

    pulse init is 1900 instead of 1100?

--- AutonomousDistroBoard/Core/Src/main.c
+++ AutonomousDistroBoard/Core/Src/main.c
@@ -386,7 +386,7 @@ static void MX_TIM11_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 1000;
+  sConfigOC.Pulse = 1900;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   if (HAL_TIM_PWM_ConfigChannel(&htim11, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
