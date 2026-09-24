150e3ad9580a3662ae51bc6a381c1720e223b828
peterstark21 <122842467+peterstark21@users.noreply.github.com>
2026-09-23 20:02:10 -0400

    Updated  if for contactor closed state

--- AutonomousDistroBoard/Core/Src/logic.c
+++ AutonomousDistroBoard/Core/Src/logic.c
@@ -196,7 +196,7 @@ void logic_run(
 			// Precharge off, contactor on
 			HAL_GPIO_WritePin(PRECHARGE_EN_GPIO_Port, PRECHARGE_EN_Pin, GPIO_PIN_RESET);
 			HAL_GPIO_WritePin(MAIN_COIL_EN_GPIO_Port, MAIN_COIL_EN_Pin, GPIO_PIN_SET);
-			if (IS_GPIO_PIN(ESTOP_CLOSED_GPIO_Port, ESTOP_CLOSED_Pin) == GPIO_PIN_SET) {
+			if (IS_GPIO_PIN(ESTOP_CLOSED_Pin) == GPIO_PIN_SET) {
 				// If the estop is closed, it means the contactor is not closed when it should be
 				logic_switch_mode(state, LOGIC_MODE_CONTACTOR_CLOSED, NOW());
 				break;
